3d599b6c0d85aa318a021c27ca62056092d2f517
carlson <eric@connectedbits.com>
2023-12-20 17:34:45 -0500

    Pant-load of built-in functions

--- lib/spot_feel/builtin_functions.rb
+++ lib/spot_feel/builtin_functions.rb
@@ -3,6 +3,13 @@
 module SpotFeel
   def self.builtin_functions
     {
+      # Conversion functions
+      "string": ->(from) { from.to_s },
+      "number": ->(from) { from.contains?(".") ? from.to_f : from.to_i },
+      # Boolean functions
+      "not": ->(value) { !value },
+      "is defined": ->(value) { !value.nil? },
+      "get or else": ->(value, default) { value.nil? ? default : value },
       # String functions
       "substring": ->(string, start, length) { string[start - 1, length] },
       "substring before": ->(string, match) { string.split(match).first },
@@ -16,6 +23,57 @@ module SpotFeel
       "matches": ->(string, match) { string.match?(match) },
       "replace": ->(string, match, replacement) { string.gsub(match, replacement) },
       "split": ->(string, match) { string.split(match) },
+      # Numeric functions
+      "decimal": ->(n, scale) { n.round(scale) },
+      "floor": ->(n) { n.floor },
+      "ceiling": ->(n) { n.ceil },
+      "round up": ->(n) { n.ceil },
+      "round down": ->(n) { n.floor },
+      "abs": ->(n) { n.abs },
+      "modulo": ->(n, divisor) { n % divisor },
+      "sqrt": ->(n) { Math.sqrt(n) },
+      "log": ->(n) { Math.log(n) },
+      "exp": ->(n) { Math.exp(n) },
+      "odd": ->(n) { n.odd? },
+      "even": ->(n) { n.even? },
+      "random": ->(n) { rand(n) },
+      # List functions
+      "list contains": ->(list, match) { list.include?(match) },
+      "count": ->(list) { list.length },
+      "min": ->(list) { list.min },
+      "max": ->(list) { list.max },
+      "sum": ->(list) { list.sum },
+      "product": ->(list) { list.inject(:*) },
+      "mean": ->(list) { list.sum / list.length },
+      "median": ->(list) { list.sort[list.length / 2] },
+      "stddev": ->(list) { Math.sqrt(list.map { |n| (n - list.sum / list.length)**2 }.sum / list.length) },
+      "mode": ->(list) { list.group_by(&:itself).values.max_by(&:size).first },
+      "all": ->(list) { list.all? },
+      "any": ->(list) { list.any? },
+      "sublist": ->(list, start, length) { list[start - 1, length] },
+      "append": ->(list, item) { list + [item] },
+      "concatenate": ->(list1, list2) { list1 + list2 },
+      "insert before": ->(list, position, item) { list.insert(position - 1, item) },
+      "remove": ->(list, position) { list.delete_at(position - 1); list },
+      "reverse": ->(list) { list.reverse },
+      "index of": ->(list, match) { list.index(match) + 1 },
+      "union": ->(list1, list2) { list1 | list2 },
+      "distinct values": ->(list) { list.uniq },
+      "duplicate values": ->(list) { list.select { |e| list.count(e) > 1 }.uniq },
+      "flatten": ->(list) { list.flatten },
+      "sort": ->(list) { list.sort },
+      "string join": ->(list, separator) { list.join(separator) },
+      # Context functions
+      "get value": ->(context, name) { context[name] },
+      "context put": ->(context, name, value) { context[name] = value },
+      "context merge": ->(context1, context2) { context1.merge(context2) },
+      # Temporal functions
+      "now": ->() { Time.now },
+      "today": ->() { Date.today },
+      "day of week": ->(date) { date.wday },
+      "day of year": ->(date) { date.yday },
+      "week of year": ->(date) { date.cweek },
+      "month of year": ->(date) { date.month },
     }
   end
 end
--- test/spot_feel/builtin_functions_test.rb
+++ test/spot_feel/builtin_functions_test.rb
@@ -9,7 +9,24 @@ module SpotFeel
   # https://docs.camunda.io/docs/components/modeler/feel/builtin-functions/feel-built-in-functions-introduction/
   #
   describe :builtin_functions do
+    describe :conversion do
+    end
+
     describe :boolean do
+      it "should eval not" do
+        # BUG: this is returning undefined method `-@' for true:TrueClass
+        #_(SpotFeel.eval('not(true)')).must_equal false
+      end
+
+      it "should eval is defined" do
+        _(SpotFeel.eval('is defined("Hello world")')).must_equal true
+        _(SpotFeel.eval('is defined(null)')).must_equal false
+      end
+
+      it "should eval get or else" do
+        _(SpotFeel.eval('get or else("Hello world", "Goodbye world")')).must_equal "Hello world"
+        _(SpotFeel.eval('get or else(null, "Goodbye world")')).must_equal "Goodbye world"
+      end
     end
 
     describe :string do
@@ -63,18 +80,205 @@ module SpotFeel
     end
 
     describe :numeric do
+      it "should eval decimal" do
+        _(SpotFeel.eval('decimal(1.234, 2)')).must_equal 1.23
+      end
+
+      it "should eval floor" do
+        _(SpotFeel.eval('floor(1.234)')).must_equal 1
+      end
+
+      it "should eval ceiling" do
+        _(SpotFeel.eval('ceiling(1.234)')).must_equal 2
+      end
+
+      it "should eval round up" do
+        _(SpotFeel.eval('round up(1.234)')).must_equal 2
+      end
+
+      it "should eval round down" do
+        _(SpotFeel.eval('round down(1.234)')).must_equal 1
+      end
+
+      it "should eval abs" do
+        _(SpotFeel.eval('abs(-1.234)')).must_equal 1.234
+      end
+
+      it "should eval modulo" do
+        _(SpotFeel.eval('modulo(5, 2)')).must_equal 1
+      end
+
+      it "should eval sqrt" do
+        _(SpotFeel.eval('sqrt(4)')).must_equal 2
+      end
+
+      it "should eval log" do
+        _(SpotFeel.eval('log(10)')).must_equal 2.302585092994046
+      end
+
+      it "should eval exp" do
+        _(SpotFeel.eval('exp(2)')).must_equal 7.38905609893065
+      end
+
+      it "should eval odd" do
+        _(SpotFeel.eval('odd(1)')).must_equal true
+        _(SpotFeel.eval('odd(2)')).must_equal false
+      end
+
+      it "should eval even" do
+        _(SpotFeel.eval('even(1)')).must_equal false
+        _(SpotFeel.eval('even(2)')).must_equal true
+      end
+
+      it "should eval random" do
+        _(SpotFeel.eval('random(10)')).must_be :<, 10
+      end
     end
 
     describe :list do
+      it "should eval list contains" do
+        _(SpotFeel.eval('list contains(["Hello", "world"], "world")')).must_equal true
+      end
+
+      it "should eval count" do
+        _(SpotFeel.eval('count(["Hello", "world"])')).must_equal 2
+      end
+
+      it "should eval min" do
+        _(SpotFeel.eval('min([1, 2, 3])')).must_equal 1
+      end
+
+      it "should eval max" do
+        _(SpotFeel.eval('max([1, 2, 3])')).must_equal 3
+      end
+
+      it "should eval sum" do
+        _(SpotFeel.eval('sum([1, 2, 3])')).must_equal 6
+      end
+
+      it "should eval product" do
+        _(SpotFeel.eval('product([1, 2, 3])')).must_equal 6
+      end
+
+      it "should eval mean" do
+        _(SpotFeel.eval('mean([1, 2, 3])')).must_equal 2
+      end
+
+      it "should eval median" do
+        _(SpotFeel.eval('median([1, 2, 3])')).must_equal 2
+      end
+
+      it "should eval stddev" do
+        #_(SpotFeel.eval('stddev([1, 2, 3])')).must_equal 0.816496580927726
+      end
+
+      it "should eval mode" do
+        _(SpotFeel.eval('mode([1, 2, 2, 3])')).must_equal 2
+      end
+
+      it "should eval all" do
+        _(SpotFeel.eval('all([true, true, true])')).must_equal true
+        _(SpotFeel.eval('all([true, false, true])')).must_equal false
+      end
+
+      it "should eval any" do
+        _(SpotFeel.eval('any([true, false, false])')).must_equal true
+        _(SpotFeel.eval('any([false, false, false])')).must_equal false
+      end
+
+      it "should eval sublist" do
+        _(SpotFeel.eval('sublist([1, 2, 3], 1, 2)')).must_equal [1, 2]
+      end
+
+      it "should eval append" do
+        _(SpotFeel.eval('append([1, 2, 3], 4)')).must_equal [1, 2, 3, 4]
+      end
+
+      it "should eval concatenate" do
+        _(SpotFeel.eval('concatenate([1, 2, 3], [4, 5, 6])')).must_equal [1, 2, 3, 4, 5, 6]
+      end
+
+      it "should eval insert before" do
+        _(SpotFeel.eval('insert before([1, 2, 3], 2, 4)')).must_equal [1, 4, 2, 3]
+      end
+
+      it "should eval remove" do
+        _(SpotFeel.eval('remove([1, 2, 3], 2)')).must_equal [1, 3]
+      end
+
+      it "should eval reverse" do
+        _(SpotFeel.eval('reverse([1, 2, 3])')).must_equal [3, 2, 1]
+      end
+
+      it "should eval index of" do
+        _(SpotFeel.eval('index of([1, 2, 3], 2)')).must_equal 2
+      end
+
+      it "should eval union" do
+        _(SpotFeel.eval('union([1, 2, 3], [4, 5, 6])')).must_equal [1, 2, 3, 4, 5, 6]
+      end
+
+      it "should eval distinct values" do
+        _(SpotFeel.eval('distinct values([1, 2, 2, 3])')).must_equal [1, 2, 3]
+      end
+
+      it "should eval duplicate values" do
+        _(SpotFeel.eval('duplicate values([1, 2, 2, 3])')).must_equal [2]
+      end
+
+      it "should eval flatten" do
+        _(SpotFeel.eval('flatten([[1, 2], [3, 4]])')).must_equal [1, 2, 3, 4]
+      end
+
+      it "should eval sort" do
+        _(SpotFeel.eval('sort([3, 2, 1])')).must_equal [1, 2, 3]
+      end
+
+      it "should eval string join" do
+        _(SpotFeel.eval('string join(["Hello", "world"], " ")')).must_equal "Hello world"
+      end
     end
 
     describe :context do
+      it "should eval get value" do
+        _(SpotFeel.eval('get value({"foo": "bar"}, "foo")')).must_equal "bar"
+      end
+
+      it "should eval context put" do
+        # BUG:
+        #_(SpotFeel.eval('context put({}, "foo", "bar")')).must_equal({ "foo" => "bar" })
+      end
+
+      it "should eval context merge" do
+        _(SpotFeel.eval('context merge({"foo": "bar"}, {"bar": "baz"})')).must_equal({ "foo" => "bar", "bar" => "baz" })
+      end
     end
 
     describe :temporal do
-    end
+      it "should eval now" do
+        # BUG: not recoginizing now() as a function (no args?)
+        #_(SpotFeel.eval('now()')).must_be_kind_of Time
+      end
 
-    describe :conversion do
+      it "should eval today" do
+        #_(SpotFeel.eval('today()')).must_be_kind_of Date
+      end
+
+      it "should eval day of week" do
+        _(SpotFeel.eval('day of week(date("1963-1-1"))')).must_equal 2
+      end
+
+      it "should eval day of year" do
+        _(SpotFeel.eval('day of year(date("1963-1-1"))')).must_equal 1
+      end
+
+      it "should eval week of year" do
+        _(SpotFeel.eval('week of year(date("1963-1-1"))')).must_equal 1
+      end
+
+      it "should eval month of year" do
+        _(SpotFeel.eval('month of year(date("1963-1-1"))')).must_equal 1
+      end
     end
 
     describe :misc do
