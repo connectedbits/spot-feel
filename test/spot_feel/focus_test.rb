1ac8f234d199e1eec5e500af09b6622b9bbe0fe1
carlson <eric@connectedbits.com>
2023-12-22 10:31:40 -0500

    Add focus test

---
+++ test/spot_feel/focus_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module SpotFeel
+  describe :focus do
+    # Using this since I couldn't get `focus` in Minitest in a gem to work
+    it "should pass this focused test" do
+    end
+  end
+end
